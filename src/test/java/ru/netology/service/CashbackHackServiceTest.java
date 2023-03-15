cbb520f57203ac143861e9ace2c1756a04a1c715
POP CODE <lolo4ka69@list.ru>
2023-03-15 22:59:28 +0300

    Commit Test

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public  class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCount1Test() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(service.remain(1000), 1000);
+
+    }
+    @Test
+    public void shouldCount1Test2() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(service.remain(10), 990);
+
+    }
+    @Test
+    public void shouldCount1Test3() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(service.remain(1500), 500);
+
+    }
+}
